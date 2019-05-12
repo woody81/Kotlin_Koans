bc711aa76d91e9ac9927bbf715ac238e6672bcb0
lesakon <ondrej.lesak@storaenso.com>
2019-05-12 21:02:33 +0200

    finalized Introduction

--- Introduction/Object expressions/src/Task.kt
+++ Introduction/Object expressions/src/Task.kt
@@ -2,6 +2,10 @@ import java.util.*
 
 fun getList(): List<Int> {
     val arrayList = arrayListOf(1, 5, 2)
-    Collections.sort(arrayList, object {})
+    Collections.sort(arrayList, object:Comparator<Int> {
+        override fun compare(o1: Int, o2: Int): Int {
+            return o2.compareTo(o1)
+        }
+    })
     return arrayList
 }
